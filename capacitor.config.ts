a7629295c2989386e98c557ae3e65b686431ae0c
guru071 <guruprasath23062008@gmail.com>
2026-07-05 11:11:36 +0530

    chore: fix invalid capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'tech.goatech.maghgo',
+  appId: 'com.maghgo.app',
   appName: 'MAGHGO',
-  webDir: 'public',
-  bundledWebRuntime: false,
+  webDir: 'out',
   server: {
     url: 'https://maghgo.goatech.tech',
     cleartext: true
